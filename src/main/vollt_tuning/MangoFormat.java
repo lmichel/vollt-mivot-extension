88a1d0d2cf07dd3126d726c8e523f92b2a4569e5
lmichel <laurent.michel@astro.unistra.fr>
2025-07-28 17:33:44 +0200

    imported from XTAPDB

---
+++ src/main/vollt_tuning/MangoFormat.java
@@ -0,0 +1,50 @@
+package main.vollt_tuning;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import tap.ServiceConnection;
+import tap.TAPException;
+import tap.TAPExecutionReport;
+import tap.data.TableIterator;
+import tap.formatter.OutputFormat;
+
+public class MangoFormat implements OutputFormat {
+
+	CustomVOTableFormat votFmt;
+	public MangoFormat(ServiceConnection service) {
+		votFmt=new CustomVOTableFormat(service);
+	}
+	
+	@Override
+	public String getMimeType() {
+		// TODO Auto-generated method stub
+		return "application/x-votable+xml;content=mivot";
+	}
+
+	@Override
+	public String getShortMimeType() {
+		// TODO Auto-generated method stub
+		return "mango";
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return votFmt.getDescription();
+	}
+
+	@Override
+	public String getFileExtension() {
+		// TODO Auto-generated method stub
+		return votFmt.getFileExtension();
+	}
+
+	@Override
+	public void writeResult(TableIterator result, OutputStream output, TAPExecutionReport execReport, Thread thread)
+			throws TAPException, IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		votFmt.writeResult(result, output, execReport, thread);
+	}
+
+}
